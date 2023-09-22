3f15ae15187c801856f301078e5eb0dd6ab70115
Bashar403 <133667040+Bashar403@users.noreply.github.com>
2023-09-22 10:12:06 -0400

    added Click events for prospect, convert, and retain

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 // WEB303 Assignment 2
 
+$(document).ready(function () {
+    // Click event for the "Prospect" link
+    $("#prospect").click(function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      loadContent("prospect.html"); // Load content from prospect.html
+    });
+  
+    // Click event for the "Convert" link
+    $("#convert").click(function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      loadContent("convert.html"); // Load content from convert.html
+    });
+  
+    // Click event for the "Retain" link
+    $("#retain").click(function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      loadContent("retain.html"); // Load content from retain.html
+    });
+  });
+
+  
+
+
 
